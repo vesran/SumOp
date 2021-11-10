4f56fb86cdcff5c8a50552ace1ce4b1a3520c91c
vesran <tran.yves@gmail.com>
2021-11-10 18:17:05 +0100

    refacto text cleaning

--- synthesis/aspects/preprocessing.py
+++ synthesis/aspects/preprocessing.py
@@ -1,15 +1,6 @@
-from symspellpy import SymSpell, Verbosity
-from unidecode import unidecode
-import pkg_resources
-import spacy
-import re
+from synthesis.text_cleaning import \
+    (load_symspell, cleaning_corpus, remove_punctuation, fix_spelling, custom_remove_stopwords)
 
-nlp = spacy.load("en_core_web_sm")
-
-
-########################################################################
-# Main functions
-########################################################################
 
 def preprocess(corpus, stops):
     sym_spell = load_symspell()
@@ -19,74 +10,3 @@ def preprocess(corpus, stops):
     corpus = [custom_remove_stopwords(s, stops) for s in corpus]
     return corpus
 
-
-def custom_remove_stopwords(sent, stops):
-    words = []
-    for w in sent.split(' '):
-        if w not in stops:
-            words.append(w)
-    return ' '.join(words)
-
-
-def cleaning_corpus(corpus):
-    docs = []
-    for document in nlp.pipe(corpus, disable=["parser", "ner", "textcat"]):
-        doc = []
-        for token in document:
-            doc.append(token.lemma_)
-        docs.append(doc)
-    return [_decontracted(' '.join(doc)) for doc in docs]
-
-
-def remove_punctuation(s):
-    s = s.lower()
-    s = unidecode(s)
-    s = re.sub('\d+', '<d>', s)
-    s = re.sub("[^a-z <d>]", ' ', s)
-    s = re.sub(' +', ' ', s)
-    s = re.sub('ooo+', 'o', s)
-    return s.strip()
-
-
-def load_symspell():
-    sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-    dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
-    # term_index is the column of the term and count_index is the
-    # column of the term frequency
-    sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
-    return sym_spell
-
-
-def fix_spelling(corpus, sym_spell):
-    clean_corpus = []
-    for doc in corpus:
-        words = []
-        for word in doc.split(' '):
-            if word == '':
-                continue
-            suggestions = sym_spell.lookup(word, Verbosity.CLOSEST, max_edit_distance=2, include_unknown=True)
-            corrected_word = next(iter(suggestions))
-            words.append(corrected_word._term)
-        clean_corpus.append(' '.join(words))
-    return clean_corpus
-
-
-########################################################################
-# Low-level functions
-########################################################################
-
-def _decontracted(phrase):
-    # specific
-    phrase = re.sub(r"won\'t", "will not", phrase)
-    phrase = re.sub(r"can\'t", "can not", phrase)
-
-    # general
-    phrase = re.sub(r"n\'t", " not", phrase)
-    phrase = re.sub(r"\'re", " are", phrase)
-    phrase = re.sub(r"\'s", " is", phrase)
-    phrase = re.sub(r"\'d", " would", phrase)
-    phrase = re.sub(r"\'ll", " will", phrase)
-    phrase = re.sub(r"\'t", " not", phrase)
-    phrase = re.sub(r"\'ve", " have", phrase)
-    phrase = re.sub(r"\'m", " am", phrase)
-    return phrase
